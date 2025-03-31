4c0b82bb901c4fd99545407f173bf36fb9863104
Noemi Collantes <noemicollantereyes24@gmail.com>
2025-03-31 12:29:22 -0500

    navbar and home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {NavbarComponent} from '../../components/navbar/navbar.component';
+import {FooterComponent} from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   imports: [
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
