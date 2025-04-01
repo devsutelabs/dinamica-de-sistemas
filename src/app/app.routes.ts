92ea5b29458fc8f73c7f9d03124077269cb5bd0d
Noemi Collantes <noemicollantereyes24@gmail.com>
2025-04-01 15:37:56 -0500

    creación de componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,31 @@
 import {RouterModule, Routes} from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import {NgModule} from '@angular/core';
+import {ContactComponent} from './pages/contact/contact.component';
+import { NewsComponent } from './pages/news/news.component';
+import { EventsComponent } from './pages/events/events.component';
+import {BooksComponent} from './pages/books/books.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'news',
+    component: NewsComponent
+  },
+  {
+    path: 'events',
+    component: EventsComponent
+  },
+  {
+    path: 'books',
+    component: BooksComponent
   }
 ];
 
