5192e9bfa9264c2c1d11087553d82fc6eefd68fe
LUZZAPARAN <lmzaparanc@ufpso.edu.co>
2025-06-05 20:51:24 -0500

    "cambios de config de login"

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
-
-import {authInterceptor} from './core/interceptors/auth-interceptor';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './core/interceptors/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(withInterceptorsFromDi()),
+    provideHttpClient(
+      withInterceptors([
+        authInterceptor
+      ])
+    ),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    {
-      provide: HTTP_INTERCEPTORS,
-      useValue: authInterceptor,
-      multi: true,
-    },
   ]
 };
--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -1,35 +1,3 @@
-/*
-import { Component } from '@angular/core';
-import {CommonModule, NgOptimizedImage} from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import {AuthService} from '../../services/auth.service';
-import {Router} from '@angular/router';
-
-@Component({
-  standalone: true,
-  selector: 'app-login',
-  templateUrl: './login.html',
-  styleUrls: ['./login.css'],
-  imports: [CommonModule, FormsModule, NgOptimizedImage],
-})
-export class Login {
-  username = '';
-  password = '';
-  error = '';
-
-  constructor(private auth: AuthService, private router: Router) {}
-
-  login(): void {
-    this.auth.login(this.username, this.password).subscribe({
-      next: () => {this.router.navigate(['/events/edit']);},
-      error: () => {this.error = 'Credenciales inválidas';}
-    });
-  }
-
-  clearError(): void {
-    this.error = '';
-  }
- */
 import {Component, OnInit} from '@angular/core';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -58,8 +26,6 @@ export class Login implements OnInit{
   }
 
   login() {
-    console.log('Intentando login', this.username, this.password);
-    // Validación manual para evitar submit vacío
     if (!this.username || !this.password) {
       this.error = 'Usuario y contraseña son obligatorios';
       return;
